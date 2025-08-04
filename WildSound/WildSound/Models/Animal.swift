4e4794597b3378773d81b16fc33bb723f725b865
AlexanderMicksch <murphman.number.one@gmail.com>
2025-08-04 11:17:50 +0200

    1.1

---
+++ WildSound/WildSound/Models/Animal.swift
@@ -0,0 +1,37 @@
+//
+//  Animal.swift
+//  WildSound
+//
+//  Created by Alexander Micksch on 04.08.25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Animal: Identifiable {
+    @Attribute(.unique) var id: UUID
+    var name: String
+    var soundURL: String
+    var wikiTitle: String
+    var isFavorite: Bool
+    var isGuessed: Bool
+    var soundSource: String?
+    
+    init(id: UUID = UUID(),
+         name: String,
+         soundURL: String,
+         wikiTitle: String,
+         isFavorite: Bool,
+         isGuessed: Bool,
+         soundSource: String? = nil)
+    {
+        self.id = id
+        self.name = name
+        self.soundURL = soundURL
+        self.wikiTitle = wikiTitle
+        self.isFavorite = isFavorite
+        self.isGuessed = isGuessed
+        self.soundSource = soundSource
+    }
+}
